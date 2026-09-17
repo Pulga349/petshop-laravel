8564b12fe0b957b280e7f775a380290aa00f8a29
Pulga349 <graciasjose13@gmail.com>
2026-09-17 03:20:15 -0300

    fix: use https origin for production assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if ($this->app->environment('production')) {
+            $appUrl = rtrim((string) config('app.url'), '/');
+            $secureAppUrl = preg_replace('/^http:\/\//i', 'https://', $appUrl);
+
             URL::forceScheme('https');
-            URL::forceRootUrl(config('app.url'));
+            URL::forceRootUrl($secureAppUrl);
+            URL::useAssetOrigin($secureAppUrl);
         }
     }
 }
